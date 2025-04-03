7aa6ee1f7511b98b670b53bd5e9f034067fc3d13
Harshita Khare <121930563+HarshitaKhare28@users.noreply.github.com>
2025-04-03 21:46:33 +0530

    Update Contact.jsx

--- client/src/Components/Contact.jsx
+++ client/src/Components/Contact.jsx
@@ -28,7 +28,7 @@ export const Contact = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/send-email', {
+      const response = await fetch('https://www.harshitakhare.co/api/send-email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
